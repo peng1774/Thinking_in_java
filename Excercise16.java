1653c3751bd708b21ded9f984fcbc19da3518a59
zhulin peng <zhulin.peng@hermes-microvision.com>
2018-07-02 18:34:30 -0700

    excercise16

---
+++ Excercise16.java
@@ -0,0 +1,44 @@
+package chap7_polymorphism;
+import static java.lang.System.out;
+
+class AlertStatus{
+	public void display(){ out.println("AlertStatus"); }
+}
+class AlertStatusIdle extends AlertStatus{
+	public void display(){ out.println("AlertStatusIdle!"); }
+}
+class AlertStatusWarning extends AlertStatus{
+	public void display(){ out.println("AlertStatusWarning!"); }
+}
+class AlertStatusActive extends AlertStatus{
+	public void display(){ out.println("AlertStatusActive!"); }
+}
+
+class StarShip{
+	AlertStatus a = new AlertStatus();
+	public void showPannel(){
+		a.display();
+	}
+	public void switchIdle(){
+		a = new AlertStatusIdle();
+	}
+	public void switchWarning(){
+		a = new AlertStatusWarning();
+	}
+	public void switchActive(){
+		a = new AlertStatusActive();
+	}
+}
+
+public class Excercise16{
+	private static StarShip s = new StarShip();
+	public static void main(String[] args){
+		s.showPannel();
+		s.switchIdle();
+		s.showPannel();
+		s.switchWarning();
+		s.showPannel();
+		s.switchActive();
+		s.showPannel();
+	}
+}
